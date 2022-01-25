730e5736a333ec72fd6ac8ec32a33c8c1f1dfd0d
Dean <dean@deansantillan.com>
2022-01-25 17:52:40 +1000

    task(): completed scraping of basic data. need to restructure but when adding to a function no data is being returned.'

--- scraper.js
+++ scraper.js
@@ -1,117 +1,76 @@
-import { table } from 'console';
 import https from 'https';
 import jsdom from 'jsdom';
 import { JSDOM } from 'jsdom';
 
 const url =
-	'https://www.atptour.com/en/rankings/singles?rankDate=2022-01-17&rankRange=0-4';
+	'https://www.atptour.com/en/rankings/singles?rankDate=2022-01-17&rankRange=0-2';
 
 const req = https.request(url, res => {
 	//
 	let htmlData = '';
-	const playerData = {};
-	const trArr = [];
 	res.on('data', chunk => {
-		//console.log(`Data chunk: ${chunk}`);
 		htmlData += chunk;
 	});
 	//
 	res.on('end', () => {
 		const dom = new JSDOM(htmlData).window.document;
+		const playerTRows = dom.querySelectorAll(
+			'#player-rank-detail-ajax >tbody > tr'
+		);
+		const playersArray = [];
 
-		const playerTRows = dom.querySelector('tbody');
-		const trows = playerTRows.querySelectorAll('td');
+		playerTRows.forEach(item => {
+			const rank = item.querySelector('.rank-cell');
+			const name = item.querySelector('.player-cell-wrapper > a');
+			const age = item.querySelector('.age-cell');
+			const flag = item.querySelector('.country-item > img');
+			const points = item.querySelector('.points-cell');
+			const tournamentsPlayed = item.querySelector('.tourn-cell > a');
+			playersArray.push({
+				name: name ? name.textContent.trim() : null,
 
-		const playerData = {};
-			trows.forEach(td => {
-				
-				switch (td.getAttribute('class')) {
-					case 'rank-cell':
-						console.log(td.textContent.trim());
-                    
-						break;
-					case 'move-cell':
-						console.log(td.nextElementSibling.textContent.trim());
-						break;
-					case 'country-cell':
-						console.log(
-							td
-								.querySelector('.country-inner > div > img')
-								.src.trim()
-						);
-						console.log(
-							td
-								.querySelector('.country-inner > div > img')
-								.alt.trim()
-						);
-						break;
-					case 'player-cell':
-						console.log(td.textContent.trim());
-						console.log(
-							td
-								.querySelector('.player-cell-wrapper > a')
-								.href.trim()
-						);
-						break;
-					case 'age-cell':
-						console.log(td.textContent.trim());
-						break;
-					case 'points-cell':
-						console.log(td.textContent.trim());
-						break;
-					case 'tourn-cell':
-						console.log(td.textContent.trim());
-						break;
-					case 'pts-cell':
-						console.log(td.textContent.trim());
-						break;
-					case 'next-cell':
-						console.log(td.textContent.trim());
-                        console.log('------------------------------')
-						break;
-				}
-			})
-            
-	;
-		//.forEach(link => {
-		// 	console.log(link);
-		// });
+				personalbio: {
+					country: '',
+					countryabbreviated: flag.alt ? flag.alt.trim() : null,
+					flag: flag.src ? flag.src.trim() : null,
+					age: age ? age.textContent.trim() : null,
+					birthplace: '',
+					birthdate: '',
+					height: '',
+					weight: '',
+					image: '',
+					socialmedia: [
+						{ facebook: '' },
+						{ twitter: '' },
+						{ instagram: '' },
+						{ youtube: '' },
+						{ web: '' },
+						{ topcourt: '' },
+					],
+				},
+				tennisbio: {
+					url: name.href ? name.href.trim() : null,
+					rank: rank ? rank.textContent.trim() : null,
+					turnedpro: null,
+					plays: '',
+					coach: [],
+					prizemoney: 0,
+					points: points ? points.textContent.trim() : null,
+					tournamentsPlayed: tournamentsPlayed
+						? tournamentsPlayed.textContent.trim()
+						: null,
+				},
+			});
+		});
 
-		// arrTableRow.forEach(item => {
-		// 	item.querySelectorAll('tr').forEach(tableRows => {
-
-		// 		tableRows.querySelectorAll('td').forEach(data => {
-
-		// 		});
-		// 	});
-		// });
-		// dom.window.document
-		// 	.querySelectorAll('.player-cell-wrapper a')
-		// 	.forEach(link => {
-		// 		console.log(link.href.trim());
-		// 	});
-
-		// dom.window.document.querySelectorAll('.age-cell').forEach(link => {
-		// 	console.log(link.textContent.trim());
-		// });
-		// fs.writeFile('ranking.html', htmlData, err => {
-		// 	if (err) {
-		// 		console.log(err);
-		// 	} else {
-		// 		//console.log('file was written');
-		// 		// fs.readFileSync('ranking.html', 'utf-8');
-		// 		// const dom = new JSDOM(htmlData);
-		// 		// dom.window.document
-		// 		// 	.querySelectorAll('.player-cell-wrapper a')
-		// 		// 	.forEach(link => {
-		// 		// 		console.log(link.textContent);
-		// 		// 	});
-		// 	}
-		// });
-		//console.log(htmlData);
-
-		//console.log('No more data.');
+		const players = {
+			playersArray,
+		};
+		console.log(players.playersArray)
+		
 	});
 });
-// .end() function is required to begin the above request.
+
 req.end();
+
+
